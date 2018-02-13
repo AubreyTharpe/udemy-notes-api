94a2959e91448492f58fe6a6534b4b6fb64a805d
Aubrey Tharpe <John.Tharpe@ibm.com>
2018-02-13 12:32:59 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-}
+var password = '123abc!';
+// var time1 = new Date();
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//         var time2 = new Date();
+//         console.log(time2 - time1);
+//     });
+// });
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var hashedPassword = '$2a$10$pc3pvJWmdUhEbuEKcYt81.m/uftnPeD46ib05jzvruU3ankL3AiJm';
 
-var decoded = jwt.verify(token + '1', '123abc');
-console.log('decoded', decoded);
+bcrypt.compare('123', hashedPassword, (err, res) => {
+    console.log(res);
+});
+// var data = {
+//     id: 10
+// }
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token + '1', '123abc');
+// console.log('decoded', decoded);
 
 // jwt.verify
 
